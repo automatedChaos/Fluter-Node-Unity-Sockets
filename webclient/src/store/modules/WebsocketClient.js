5d29d0f271ebf0bc96f5c185c383af035b7fd75c
Alcwyn Parker <alcwyn.parker@falmouth.ac.uk>
2021-04-04 00:02:02 +0100

    Sorted the port number in the client

--- webclient/src/store/modules/WebsocketClient.js
+++ webclient/src/store/modules/WebsocketClient.js
@@ -1,7 +1,7 @@
 // initial state
 // shape: [{ id, quantity }]
 const state = () => ({
-  serverAddress: 'ws://34.80.38.153:8083',
+  serverAddress: 'ws://34.80.38.153:8081',
   socket: null,
   isConnected: false,
   connectionKey: 'null',
